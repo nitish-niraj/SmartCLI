13ecef55b2a77e489b22a9858e8ab99791ad2d9e
SmartCLI Dev Team <dev@smartcli.local>
2026-05-10 18:04:39 +0530

    refactor: remove deprecated legacy integration modules and associated configuration files

--- SmartCLI-Submission/src/main/java/com/lpu/smartcli/core/Command.java
+++
@@ -1,7 +0,0 @@
-package com.lpu.smartcli.core;
-import com.lpu.smartcli.data.FileSystem;
-
-public interface Command {
-    void execute(String[] args, FileSystem fs);
-    String getDescription();
-}
--- SmartCLI-Submission/src/main/java/com/lpu/smartcli/utils/AppLogger.java
+++
@@ -1,8 +0,0 @@
-package com.lpu.smartcli.utils;
-
-/**
- * Placeholder for centralized application logging.
- */
-public class AppLogger {
-    // TODO: Implement SLF4J + Logback backed logger utilities.
-}
